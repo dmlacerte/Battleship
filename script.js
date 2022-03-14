cb1375cb984445a72a9aff514632c5bc12b0c22d
dmlacerte <dmlacerte@gmail.com>
2022-03-14 10:32:02 -0400

    Improved CSS

--- script.js
+++ script.js
@@ -1,74 +1,3 @@
-/*Game Rules:
-
-Objective: 
-Be the first to sink all of your opponent's ships.
-
-Contents: 
-3 ships each (6 total), 
-1 opponent board (hits - red, misses - gray, hidden ships - no color), 
-1 player board (blue - ships, hits - red, misses - gray)
-
-Rules:
-1. Each player places their ships on the board horizontally or vertically
-(this position cannot be changed once placed). <Add Figure with Example>
-< Stretch - Start with randomly generated ships, but later add ability to select ship, 
-and then select position using an arrow. >
-2. First player selects a spot on the opponent's board. If the shot is a hit,
-the spot will turn red. If it is a miss, the spot will turn gray.
-< Stretch - hits yellow until full ship is sunk, then turns red. >
-< Stretch - indicate which ship was hit, and change that ship in the DOM. >
-3. After the player goes, the computer takes a turn. 
-< Stretch - computer will act strategically instead of selecting a random open spot. >
-
-*/
-
-/*Layout:
-
-Boards: 
-5x5 boards 
-< Stretch - customizable board size >  
-Left border has letters, top border has numbers 
-Under / Above boards - three ships, names of ship underneath 
-< Stretch - bottom is buttons for setting ships >
-< Stretch - ships has dots representing number of spaces, will change colors when hit > 
-< Stretch - adding animations when a ship is hit > 
-Right side of boards - start game button, reset button
-< Stretch - arrows for setting ships >
-
-*/
-
-/*Design Goals:
-
-MVP (HTML):
-
-MVP Goals (CSS):
-
------
-
-Stretch Goals (HTML):
-
-Stretch Goals (CSS):
-
-*/
-
-/*Player Side JS Design: 
-
-MVP (JS):
-
-Stretch Goals (JS):
-
-*/
-
-/*Computer Side JS Design: 
-
-MVP Goals (JS):
-
-Stretch Goals (JS):
-
-*/
-
-
-
 //OVERALL GAME SETUP
 //1. Set up board
 //a) Use a constructor to set up arrays of player & computer ship objects
@@ -516,59 +445,6 @@ function computerMovesRandomly() {
     }
 }
 
-/* Variables:
-isThisFirstMove = true (default)
-firstShot = # (set from random move)
-firstShotShip = "" (set from random move)
-lastShot = # 
-lastShotWasHit = false
-lastShotShip = ""
-lastDirectionIndex = #
-moveStartingSpace = firstShot
-
-Every Time:
-    Make valid directions array using moveStartingSpace.
-
-First Hit:
-    CONDITIONS: isThisFirstMove = true
-    1. Select a random valid direction from the array.
-    UPDATE: isThisFirstMove = false
-
-Not First Hit:
-    CONDITIONS: isThisFirstMove = false
-    Choose Direction:
-        If current space is first space, and hit length = 1, choose random direction
-        If current space is first space, and hit length > 1, choose opposite direction of last direction.
-        If current space is not first space, continue in last direction. 
-        A. If last shot was a hit, and current spaces is not first space, keep direction the same.
-            CONDITIONS: lastShotWasHit = true
-            UPDATE DIRECTION: currentDirection = lastDirectionIndex
-        B. If last shot was not a hit, but have made more than one hit, direction should be opposite.
-            CONDITIONS: lastShotWasHit = false; hitSpaces > 1
-            UPDATE DIRECTION: currentDirection = lastDirectionIndex.opposite
-        C. If neither of the above are true, choose a new direction from the valid array.
-            UPDATE DIRECTION: currentDirection = random from valid directions array
-
-Move One In Direction From SPACE:
-    UPDATE: lastShot, lastDirectionIndex
-    A. If A Hit
-        1. Refresh valid directions array.
-            UPDATE: lastShotWasHit = true; lastShotShip
-        2. Check if direction is still valid, and that ship hit is the same.
-            CONDITIONS: avail spaces in direction > 0; firstShotShip = lastShotShip
-            If True:  
-                UPDATE: moveStartingSpace = firstShot
-            If False: 
-                UPDATE: moveStartingSpace = lastShot
-    B. If A Miss 
-        UPDATE: moveStartingSpace = firstShot; lastShotWasHit = false
-
-Once Ship Is Sunk:
-    Remove hit spaces from Comp potential space array
-    Reset default variables
-    Check if there are any other unsunk ships with hits
-*/
-
 let pursueHitShip = false; //Set in random move
 let isThisFirstMove = true;
 let firstShot = 0; //Set in random move
@@ -648,6 +524,7 @@ function computerMovesStrategically() {
     }
 
     //NEXT STEP, VALIDATE IF SHIP SUNK, IF SUNK AND OTHER SHIP HAS HITS, MOVE TO THAT SHIP
+    //MAKE SURE ARR IS UPDATED COMPLETELY AND SHOTS ARE REMOVED FROM COMP POTENTIAL MOVES ARRAY
 
     //Execute if shot is a miss
     if (playerBoardArray[lastShot].isTaken === false) {
@@ -660,105 +537,6 @@ function computerMovesStrategically() {
     }  
 
 }
-
-// let lastHit;
-// let hitCount = 1;
-// let pursueHitShip = false;
-// let pursueHitShipDirectionIndex;
-// let reverseDirection = false;
-
-// function computerMovesStrategically() {
-//     // playerBoardArray[lastHit].updateSurroundingSpace(playerBoardArray);
-//     let shipHit = playerBoardArray[lastHit].isTakenShipName;
-//     let shipHitIndex = shipsArrPlay.findIndex(ship => ship.name === shipHit);
-//     let directionIndex;
-//     let nextMove;
-
-//     //If first hit, choose a random valid direction to pursue ship
-//     if (!(pursueHitShipDirectionIndex + 1)) {
-//         //Determine which directions are valid based on the length of the ship hit
-
-//         let potentialDirectionIndexArray = [];
-//         playerBoardArray[lastHit].updateSurroundingSpace(playerBoardArray);
-
-//         let calcHorizontalAvailability = playerBoardArray[lastHit].availCompShotsLeft + playerBoardArray[lastHit].availCompShotsRight + 1;
-//         console.log(calcHorizontalAvailability);
-//         let calcVerticalAvailability = playerBoardArray[lastHit].availCompShotsTop + playerBoardArray[lastHit].availCompShotsBottom + 1;
-//         console.log(calcVerticalAvailability);
-
-//         if (calcHorizontalAvailability >= shipsArrPlay[shipHitIndex].length) {
-//             if (!(playerBoardArray[lastHit].colNum === 5)) {
-//                 potentialDirectionIndexArray.push(0);
-//             }
-//             if (!(playerBoardArray[lastHit].colNum === 1)) {
-//                 potentialDirectionIndexArray.push(1);
-//             }
-//         }
-//         if (calcVerticalAvailability >= shipsArrPlay[shipHitIndex].length) {
-//             if (!(playerBoardArray[lastHit].rowNum === 1)) {
-//                 potentialDirectionIndexArray.push(2);
-//             }
-//             if (!(playerBoardArray[lastHit].rowNum === 5)) {
-//                 potentialDirectionIndexArray.push(3);
-//             }
-//         }
-
-//         //Randomly select a valid direction and move 1 space in that direction
-//         directionIndex = potentialDirectionIndexArray[Math.floor(Math.random() * potentialDirectionIndexArray.length)];
-//         nextMove = moveOverOneSpace(lastHit, directionIndex);
-//         console.log(nextMove);
-//     }
-//     //If second hit, pursue that direction (or the opposite direction)
-//     else if (!reverseDirection) {
-//         directionIndex = pursueHitShipDirectionIndex;
-//         nextMove = moveOverOneSpace(lastHit, directionIndex);
-//     } else if (reverseDirection) {
-//         directionIndex = pursueHitShipDirectionIndex;
-//         nextMove = moveOverOneSpace(lastHit, directionIndex);
-//         for (let i = 0; i <= hitCount; i++) {
-//             nextMove = moveOverOneSpace(nextMove, directionIndex);
-//         }
-//     }
-
-//     let domSpaceID = `#pl-${nextMove}`;
-//     let domSpace = document.querySelector(domSpaceID);
-
-//     if (playerBoardArray[nextMove].isTaken === false) {
-//         domSpace.classList.add("miss");
-//         playerBoardArray[nextMove].isMiss = true;
-//         if (pursueHitShipDirectionIndex + 1) {
-//             pursueHitShipDirectionIndex = shipsArrPlay[pursueHitShipDirectionIndex].reverseDirectionIndex;
-//             reverseDirection = true;
-//         }
-//     } else if (playerBoardArray[nextMove].isTaken === true) {
-//         //Mark move as a hit and increase hit count.
-//         hitCount++;
-//         domSpace.classList.remove("player-setup");
-//         domSpace.classList.add("hit");
-//         playerBoardArray[nextMove].isHit = true;
-//         //If current move is at an end row or column, change direction.
-//         let reachedEndCol = ((pursueHitShipDirectionIndex === 0 || pursueHitShipDirectionIndex === 1) && playerBoardArray[nextMove].isEndCol === true);
-//         let reachedEndRow = ((pursueHitShipDirectionIndex === 2 || pursueHitShipDirectionIndex === 3) && playerBoardArray[nextMove].isEndRow === true);
-//         if (reachedEndCol || reachedEndRow) {
-//             pursueHitShipDirectionIndex = shipsArrPlay[pursueHitShipDirectionIndex].reverseDirectionIndex;
-//             reverseDirection = true;
-//         }
-//         //Check if ship hit is same ship as last shot 
-//         let hitShip = playerBoardArray[nextMove].isTakenShipName;
-//         if (hitShip === shipHit) {
-//             lastHit = nextMove;
-//             pursueHitShipDirectionIndex = directionIndex;
-//         }
-//         //Check if targeted ship is sank.
-//         let isShipSank = checkIfSank(hitShip, nextMove, shipsArrPlay);
-//         console.log(isShipSank);
-//         if (isShipSank) {
-//             console.log("Ship is sank.")
-//             pursueHitShip = false;
-//         }
-//         //Reset unless there is another unsunk ship with hits
-//     }
-// }
 
 //ENDING GAME
 let playerWins = false;
